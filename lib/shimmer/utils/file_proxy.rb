cc327fe6a9d1f143e8c7992ccd665eebc540ac3e
Justin Marsh <jmarsh24@gmail.com>
2023-12-16 12:48:35 +0100

    update

--- lib/shimmer/utils/file_proxy.rb
+++ lib/shimmer/utils/file_proxy.rb
@@ -2,7 +2,7 @@
 
 module Shimmer
   class FileProxy
-    attr_reader :blob_id, :resize
+    attr_reader :blob_id, :resize, :quality
 
     delegate :message_verifier, to: :class
     delegate :content_type, :filename, to: :blob
@@ -12,33 +12,31 @@ module Shimmer
         blob_id, resize = message_verifier.verified(id)
         new blob_id: blob_id, resize: resize
       end
+
+      def message_verifier
+        @message_verifier ||= ApplicationRecord.signed_id_verifier
+      end
     end
 
-    def initialize(blob_id:, resize: nil, width: nil, height: nil)
+    def initialize(blob_id:, resize: nil, width: nil, height: nil, quality: nil)
       @blob_id = blob_id
       if !resize && width
         resize = if height
-          "#{width}x#{height}>"
+          [width, height]
         else
-          "#{width}x"
+          [width, nil]
         end
       end
       @resize = resize
-    end
-
-    class << self
-      def restore(id)
-        blob_id, resize = message_verifier.verified(id)
-        new blob_id: blob_id, resize: resize
-      end
+      @quality = quality
     end
 
     def path
-      Rails.application.routes.url_helpers.file_path(id, locale: nil)
+      Rails.application.routes.url_helpers.file_path("#{id}.#{file_extension}", locale: nil)
     end
 
     def url(protocol: Rails.env.production? ? :https : :http)
-      Rails.application.routes.url_helpers.file_url(id, locale: nil, protocol: protocol)
+      Rails.application.routes.url_helpers.file_url("#{id}.#{file_extension}", locale: nil, protocol: protocol)
     end
 
     def blob
@@ -50,7 +48,20 @@ module Shimmer
     end
 
     def variant
-      @variant ||= resizeable ? blob.representation(resize: resize).processed : blob
+      resize_array = process_resize(resize)
+
+      transformation_options = {resize_to_limit: resize_array, format: :webp}
+      transformation_options[:quality] = quality if quality
+
+      @variant ||= resizeable ? blob.representation(transformation_options).processed : blob
+    end
+
+    def variant_content_type
+      resizeable ? "image/webp" : content_type
+    end
+
+    def variant_filename
+      resizeable ? "#{filename.base}.webp" : filename.to_s
     end
 
     def file
@@ -59,8 +70,24 @@ module Shimmer
 
     private
 
+    def process_resize(resize)
+      return nil unless resize
+
+      if resize.is_a?(String)
+        # Split the string and convert to integers. If height is missing, it will be nil
+        dimensions = resize.split("x").map { |dim| dim.empty? ? nil : dim.to_i }
+        (dimensions.length == 1) ? [dimensions.first, nil] : dimensions
+      elsif resize.is_a?(Array)
+        resize
+      end
+    end
+
     def id
-      @id ||= message_verifier.generate([blob_id, resize])
+      @id ||= message_verifier.generate([blob_id, resize, quality])
+    end
+
+    def file_extension
+      File.extname(variant_filename).from(1)
     end
   end
 end
